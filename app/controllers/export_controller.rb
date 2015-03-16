3af03a959d84df14f10867552bfc79b8662dd348
Antonio Grass <antonio_grass@hotmail.com>
2015-03-15 21:39:37 -0300

    chage

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -1,11 +1,12 @@
+require 'oauth2'
+require 'net/http'
+require 'json'
+require 'axlsx'
+
 class ExportController < ApplicationController
-  require 'oauth2'
-  require 'net/http'
-  require 'json'
-  require 'axlsx'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
   def chilexpress
     token = get_access_token(params)
@@ -39,21 +40,6 @@ class ExportController < ApplicationController
 
   end
 
-  def generate_order_xls(orders)
-    Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(:name => "DATOS") do |sheet|
-        sheet.add_row ['PRODUCTO',  'SERVICIO',' DESTINO/COMUNA',  'PESO',  'DESTINATARIO',  'CALLE', 'NUMERO',  'COMPLEMENTO', 'REFERENCIA',  'ALTO', ' ANCHO', 'LARGO', 'CARGOEMPRESA',  'VALOR', 'CASILLA', 'EMAIL', 'CELULAR', 'ENTREGAOFICINA',  'INFOADICIONAL', 'AGRUPADA',  'AGRTOTALPIEZAS',  'AGRPIEZANUMERO', 'EMPRESA', 'DESTINATARIOSECUNDARIO']
-        orders.each do |order|
-          p order["_embedded"]["contact"]["name"]
-          p order["_embedded"]["address"]["street"]
-          address = process_address(order)
-          sheet.add_row ['3', '3', address[0], '1,2', order["_embedded"]["contact"]["name"], address[1], address[2], address[3], address[4], '11', '26', '29', '', '', '', ''  ]
-        end 
-      end
-      p.serialize('plantilla_chilexpress.xlsx')
-    end
-  end
-
   def process_address(order)
     comuna = order["_embedded"]["address"]["locality_name"]
     calle_temp = order["_embedded"]["address"]["street"].split(" ")
@@ -78,5 +64,4 @@ class ExportController < ApplicationController
   end
 
 
-end
 end
