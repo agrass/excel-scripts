85d1d396158b384ff40a4076b7c55ad3238ccd26
Antonio Grass <antonio_grass@hotmail.com>
2015-06-01 23:56:13 -0300

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'chilexpress' => 'export#chilexpress', as: :chilexpress
+  get 'list' => 'export#list', as: :list
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
